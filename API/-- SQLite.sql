c4f89b8fe915ebb45813dff11447ff9103093429
K300044 <K300044@D4LV8RC2.deluxe.com>
2021-11-22 07:47:18 -0600

    walking skeleton p-1

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, Username)
+VALUES (1, "Bob");
+
+INSERT INTO Users (Id, Username)
+VALUES (2, "Tom");
+
+INSERT INTO Users (Id, Username)
+VALUES (3, "Jane");
---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using System.Collections.Generic;
+using API.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id) => await _context.Users.FindAsync(id);
+    }
+}
