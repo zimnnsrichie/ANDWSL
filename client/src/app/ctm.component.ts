29833254fc9d78af34de92b44a932f01fc817165
K300044 <K300044@D4LV8RC2.deluxe.com>
2021-11-23 05:51:36 -0600

    walking skeleton p-2

---
+++ client/src/app/ctm.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ctm-root',
+    templateUrl: './ctm.component.html'
+})
+
+export class CtmComponent{
+    title: "Custom Title";
+}
+
